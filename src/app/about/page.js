475185187e25df7df590e2947b489edbdebd36e2
5h140w <5h140w23@gmail.com>
2024-08-08 20:01:38 +0100

    page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,14 +1,17 @@
 import Navbar from "@/components/Navbar";
+import React from "react";
 import styles from "@/app/Home.module.css";
 
-export default function Home() {
+const About = () => {
   return (
     <div className={styles.container}>
       <Navbar />
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to my Portfolio</h1>
-        <p>My name is XXXXX XXXXX</p>
+        <h1 className={styles.title}>About me</h1>
+        <p>I am a software engineer with expertises in JS</p>
       </main>
     </div>
   );
-}
+};
+
+export default About;
